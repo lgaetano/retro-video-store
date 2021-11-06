deef5a1c45c0cf449b3bfbb25d7862c2277738b0
Lia Gaetano <lgaetano19@gmail.com>
2021-11-06 16:58:38 -0700

    Adds registered_at time to Customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,17 +1,19 @@
 from app import db
+from datetime import datetime #date.timetuple()? POST
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
+    registered_at = db.Column(db.String)
     postal_code = db.Column(db.String)
     phone = db.Column(db.String)
     # videos = db.Column(db.ForeignKey)
-    # registered_at = db.Column(db.DateTime)
     
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
+            "registered_at": self.registered_at,
             "postal_code": self.postal_code,
             "phone": self.phone
         }
