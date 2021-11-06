1181360c8a7290430ff40219e8847b0b27da295f
Lia Gaetano <lgaetano19@gmail.com>
2021-11-05 23:22:22 -0700

    Adds get_customer_by_id route to customers_routes

--- routes/customer_routes.py
+++ routes/customer_routes.py
@@ -4,15 +4,28 @@ from app.models.rental import Rental
 from app.models.video import Video
 from flask import Blueprint, jsonify,request, make_response, abort 
 
-customers_bp = Blueprint("customers", __name__,url_prefix="/customers")
+customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
+
+
+# TODO ZIP CODE VALIDATION,
+#       PHONE NUM VALIDATION,
 
 @customers_bp.route("", methods=["GET"])
 def get_all_customer():
     """Retrieves all customers from database."""
     customers = Customer.query.all()
 
-    customer_response = []
-    for customer in customers:
-        customer_response.append(customer.to_dict())
+    return jsonify([customer.to_dict() for customer in customers]), 200
 
-    return jsonify(customer_response), 200
+@customers_bp.route("<customer_id>", methods=["GET"])
+def get_customer_by_id(customer_id):
+    """Retreives customer data by id."""
+    # TODO: Refactor 404 for JSON
+    customer = Customer.query.get_or_404(customer_id)
+
+    return jsonify(customer.to_dict())
+
+@customers_bp.route("<customer_id>", methods=["PUT"])
+def update_customer_by_id(customer_id):
+    """Updates all customer data by id"""
+    pass
