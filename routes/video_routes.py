5ebec49813ecb298390e82326113f2999c05a839
Lia Gaetano <lgaetano19@gmail.com>
2021-11-09 03:21:38 -0800

    Adds get_video_by_id to video_routes

--- routes/video_routes.py
+++ routes/video_routes.py
@@ -7,13 +7,13 @@ from flask import Blueprint, jsonify,request, make_response, abort
 rentals_bp = Blueprint("rentals",__name__,url_prefix="/rentals")
 videos_bp = Blueprint("videos",__name__,url_prefix="/videos")
 
-def validate_id(id, param_id):
+def validate_id(id):
     """Validates id for endpoint is an integer."""
     try:
         int(id)
     except:
         # abort(jsonify({f"details": "{param_id} must be an int."}), 400) # TODO: Why didn't this work?
-        abort(make_response({f"details": "{param_id} must be an int."}, 400))
+        abort(make_response({f"details": "Endpoint must be an int."}, 400))
 
 @videos_bp.route("", methods=["GET"])
 def get_all_videos():
@@ -23,12 +23,16 @@ def get_all_videos():
     return jsonify([videos.to_dict() for video in videos]), 200
 
 @videos_bp.route("<video_id>", methods=["GET"])
-def get_video_by_id(customer_id):
+def get_video_by_id(video_id):
     """Retreives customer data by id."""
+    validate_id(video_id)
     # TODO: Refactor 404 for JSON
-    video = Video.query.get_or_404(customer_id)
+    video = Video.query.get_or_404(video_id)
 
-    return jsonify(video.to_dict())
+    if not video:
+        return jsonify({"message":f"Video {video_id} was not found"}), 404
+
+    return jsonify(video.to_dict()), 200
 
 
 POST /videos
