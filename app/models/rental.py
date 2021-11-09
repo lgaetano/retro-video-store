206020fd8c0c702691acbe90ca8b27b736a9cf46
Lia Gaetano <lgaetano19@gmail.com>
2021-11-09 11:33:18 -0800

    Adds caclulate_due_date method to Rental

--- app/models/rental.py
+++ app/models/rental.py
@@ -1,7 +1,24 @@
 from app import db
+from app.models.customer import Customer
+from app.models.video import Video
+from datetime import date, timedelta
 
 class Rental(db.Model):
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), primary_key=True, nullable=False)
     video_id = db.Column(db.Integer, db.ForeignKey('video.id'), primary_key=True, nullable=False)
     due_date = db.Column(db.DateTime)
     
+    def calculate_due_date(self):
+        """Calculates due date as seven days from today. """
+        today = date.today()
+        due_date = str(today + timedelta(days=7))
+        return due_date
+
+    def checkout_to_dict(self):
+        return {
+            "customer_id": self.customer_id,
+            "video_id": self.video_id,
+            "due_date": self.calculate_due_date(),
+            "videos_checked_out_count": Customer.videos_checked_out_count,
+            "available_inventory": Video.total_inventory - Customer.videos_checked_out_count
+        }
