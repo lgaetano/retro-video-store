f142dcb1b753855ee2762385a62d8670755525fa
Lia Gaetano <lgaetano19@gmail.com>
2021-11-09 10:52:47 -0800

    Updates Customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -7,6 +7,7 @@ class Customer(db.Model):
     registered_at = db.Column(db.String)
     postal_code = db.Column(db.String)
     phone = db.Column(db.String)
+    videos_checked_out_count = db.Column(db.Integer)
     videos = db.relationship("Video", secondary="rentals", backref="videos")
     
     def to_dict(self):
