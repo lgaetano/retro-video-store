10794855db460391a36b9a9420bb4287706fe75a
Lia Gaetano <lgaetano19@gmail.com>
2021-11-09 03:17:36 -0800

    Creates VIDEO updates_from_dict method

--- app/models/video.py
+++ app/models/video.py
@@ -7,9 +7,18 @@ class Video(db.Model):
     release_date = db.Column(db.DateTime)
 
     def to_dict(self):
+        """Returns model attributes in dict form."""
         return{
             "id": self.id,
             "title": self.title,
             "release_date": self.release_date,
             "total_inventory": self.inventory
         }
+
+    def updates_from_dict(self, data):
+        """
+        Updates attributes from user data, restricting access to attributes
+        that are columns."""
+        for key, value in data.items():
+            if key in Video.__table__.columns.keys():
+                setattr(self, key, value)
