cc9b043557c990863d1109c2a40caba1e280a072
Lia Gaetano <lgaetano19@gmail.com>
2021-11-11 12:51:43 -0800

    Creates utils folder for validation functions

--- utils/validation_decorators.py
+++ utils/validation_decorators.py
@@ -1,53 +1,63 @@
 from functools import wraps
-from flask import jsonify
+from flask import jsonify, make_response, abort
 from app.models.customer import Customer
 from app.models.video import Video
 
-def validate_user_input_customer(user_input):
-    """
-    Decorator to validate user input for customer endpoints.
-    Returns JSON and 400 if bad data."""
+
+def validate_endpoint_id_is_int(id):
+    """Validates id for endpoint is an integer."""
+    try:
+        int(id)
+    except:
+        abort(make_response({f"details": "Endpoint must be an int."}, 400))
+
+# def validate_user_input_customer(user_input):
+#     """
+#     Decorator to validate user input for customer endpoints.
+#     Returns JSON and 400 if bad data."""
     
-    """Uses regex to confirm phone data matches standard US phone number."""
-    basic_phont_num = re.compile("/(\d{3}\)\s\d{3}-\d{4}")
-    if re.fullmatch(basic_phont_num, phone_num):
-        return True
-    else:
-        return False
+#     """Uses regex to confirm phone data matches standard US phone number."""
+#     basic_phont_num = re.compile("/(\d{3}\)\s\d{3}-\d{4}")
+#     if re.fullmatch(basic_phont_num, phone_num):
+#         return True
+#     else:
+#         return False
 
     
-    """Uses regex to confirm zipcode data matches standard US zipcode."""
-    basic_zipcode = re.compile("\d{5}")
-    if re.fullmatch(basic_zipcode, zip_code):
-        return True
-    return False
+#     """Uses regex to confirm zipcode data matches standard US zipcode."""
+#     basic_zipcode = re.compile("\d{5}")
+#     if re.fullmatch(basic_zipcode, zip_code):
+#         return True
+#     return False
 
 
 
-def require_instance_or_404(endpoint):
+def validate_kwarg(endpoint):
     """
-    Decorator to validate that a requested id of input data exists.
-    Returns JSON and 404 if not found."""
+    Decorator to validate that a requested id of input data is and int 
+    and that the isntance exists. Returns JSON and 404 if not found."""
     @wraps(endpoint) # Makes fn look like func to return
     def fn(*args, **kwargs):
-        if "task_id" in kwargs:
-            task_id = kwargs.get("task_id", None)
-            task = Task.query.get(task_id)
+        if "video_id" in kwargs:
+            video_id = kwargs.get("video_id", None)
+            validate_endpoint_id_is_int(video_id)
 
-            if not task:
+            video = Video.query.get(video_id)
+            if not video:
                 return jsonify(None), 404 # null
 
-            kwargs.pop("task_id")
-            return endpoint(*args, task=task, **kwargs)
+            kwargs.pop("video_id")
+            return endpoint(*args, video=video, **kwargs)
         
-        elif "goal_id" in kwargs:
-            goal_id = kwargs.get("goal_id", None)
-            goal = Goal.query.get(goal_id)
+        elif "customer_id" in kwargs:
+            customer_id = kwargs.get("customer_id", None)
+            validate_endpoint_id_is_int(customer_id)
 
-            if not goal:
+            customer = Customer.query.get(customer_id)
+            if not customer:
                 return jsonify(None), 404
 
-            kwargs.pop("goal_id")
-            return endpoint(*args, goal=goal, **kwargs)
+            kwargs.pop("customer_id")
+            return endpoint(*args, customer=customer, **kwargs)
 
     return fn
