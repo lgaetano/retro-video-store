72a0fcdc771e233c8d74b01610074d53085bba54
Lia Gaetano <lgaetano19@gmail.com>
2021-11-10 17:17:33 -0800

    Refactors rental route validation to helper func rentals_validate_input

--- routes/rental_routes.py
+++ routes/rental_routes.py
@@ -6,27 +6,39 @@ from flask import Blueprint, jsonify,request, make_response, abort
 
 rentals_bp = Blueprint("rentals", __name__, url_prefix="/rentals")
 
-@rentals_bp.route("/check-out", methods=["POST"])
-def checkout():
-    """Checksout a video to a customer and updates the database."""
-    response_body = request.get_json()
-
+def rentals_validate_input(data):
+    """
+    Function that validates presence of all mandatory fields, existence
+    of customer and video instances, and presence of rental."""
     # Confirm all mandatory fields present
     mandatory_fields = ["customer_id", "video_id"]
     for field in mandatory_fields:
-        if field not in response_body:
-            return jsonify({"message": f"Request must include {field}."}), 400
+        if field not in data:
+            # return jsonify({"message": f"Request must include {field}."}), 400
+            abort(make_response({"message": f"Request must include {field}."}, 400))
     
     # Confirm respective instances of Customer and Video exist
-    customer = Customer.query.get(response_body["customer_id"])
+    customer = Customer.query.get(data["customer_id"])
     if not customer:
-        return jsonify({"message": f"Could not locate customer {response_body['customer_id']}"}), 404
-    video = Video.query.get(response_body["video_id"])
+        # return jsonify({"message": f"Could not locate customer {response_body['customer_id']}"}), 404
+        abort(make_response({"message": f"Could not locate customer {data['customer_id']}"}, 404))
+    video = Video.query.get(data["video_id"])
     if not video:
-        return jsonify({"message": f"Could not locate video {response_body['video_id']}"}), 404
+        # return jsonify({"message": f"Could not locate video {response_body['video_id']}"}), 404
+        abort(make_response({"message": f"Could not locate video {data['video_id']}"}, 404))
     
     # Confirm rental does not already exist
     rental = Rental.query.get((customer.id, video.id))
+
+    return rental
+
+
+@rentals_bp.route("/check-out", methods=["POST"])
+def check_out():
+    """Checks out a video to a customer and updates the database."""
+    response_body = request.get_json()
+    rental = rentals_validate_input(response_body)
+    
     if rental: # exists
         return jsonify({"message": f"Could not perform checkout"}), 400
 
@@ -38,3 +50,14 @@ def checkout():
     db.session.commit()
     
     return jsonify(new_rental.checkout_to_dict()), 200
+
+@rentals_bp.route("/check-in", methods=["POST"])
+def check_in():
+    """Checks in a video from a customer and updates the database."""
+    response_body = request.get_json()
+
+    rental = rentals_validate_input(response_body)
+    if not rental:
+        return jsonify({"message": f"No outstanding rentals for customer {response_body['customer_id']} and video {response_body['video_id']}"}), 400
+
+    return jsonify(rental.check_in_to_dict()), 200
