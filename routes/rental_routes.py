06a8f032c84cdca5c0b59513a99174144dc36863
Lia Gaetano <lgaetano19@gmail.com>
2021-11-13 11:21:41 -0800

    Refactors using validate_video_customer_existence

--- routes/rental_routes.py
+++ routes/rental_routes.py
@@ -3,6 +3,7 @@ from app.models.customer import Customer
 from app.models.rental import Rental
 from app.models.video import Video
 from flask import Blueprint, jsonify,request, make_response, abort 
+from datetime import date, timedelta
 
 rentals_bp = Blueprint("rentals",__name__,url_prefix="/rentals")
 
@@ -19,38 +20,30 @@ def validate_video_customer_existence(request_body):
     customer = Customer.query.get(request_body["customer_id"])
     
     if not video:
-        return jsonify({"details":f"Video with id number {request_body['video_id']} was not found"}),404
+        abort(make_response({"details":f"Video with id number {request_body['video_id']} was not found"},404))
     elif not customer:
-        return jsonify({"details":f"Customer with id number {request_body['customer_id']} was not found"}),404
+        abort(make_response({"details":f"Customer with id number {request_body['customer_id']} was not found"},404))
 
     return customer, video
 
-
 @rentals_bp.route("/check-out",methods=["POST"])
 def checkout_video():
+    """Checks out a video to a customer, creating a new rental instance."""
     request_body = request.get_json()
     
     validate_request_body(request_body)
-    customer, video = validate_video_customer_existence(request_body)
+    validate_video_customer_existence(request_body)
 
-    video= Video.query.get(request_body["video_id"])
-    customer = Customer.query.get(request_body["customer_id"])
-    
-    if not video:
-        return jsonify({"details":f"Video with id number {request_body['video_id']} was not found"}),404
-    elif not customer:
-        return jsonify({"details":f"Customer with id number {request_body['customer_id']} was not found"}),404
     rental = Rental.query.get((request_body['video_id'], request_body['customer_id']))
-
     if rental:
         return jsonify({"message":"Could not perform checkout"}),400
-        
         
     # if video.available_inventory == 0:
     #     return jsonify({"message":"Could not perform checkout"}),400
     new_rental= Rental(
         customer_id = request_body["customer_id"],
-        video_id = request_body["video_id"]
+        video_id = request_body["video_id"],
+        checkout_date = date.today()
     )
     db.session.add(new_rental)
     db.session.commit()
@@ -58,19 +51,12 @@ def checkout_video():
 
 @rentals_bp.route("/check-in",methods=["POST"])
 def check_in_video():
+    """Checks-in a video, deleting a new rental instance."""
     request_body = request.get_json()
     validate_request_body(request_body)
-    
-    video= Video.query.get(request_body["video_id"])
-    customer = Customer.query.get(request_body["customer_id"])
-    
-    if not video:
-        return jsonify({"details":f"Video with id number {request_body['video_id']} was not found"}),404
-    elif not customer:
-        return jsonify({"details":f"Customer with id number {request_body['customer_id']} was not found"}),404
-    rental = Rental.query.get((request_body["video_id"],request_body["customer_id"]))
-    # rental = Rental.query.get(video.id,customer.id)
+    validate_video_customer_existence(request_body)
 
+    rental = Rental.query.get((request_body["video_id"],request_body["customer_id"]))
     if not rental:
         return jsonify({"message":f"No outstanding rentals for customer {request_body['customer_id']} and video {request_body['video_id']}"}),400
     
